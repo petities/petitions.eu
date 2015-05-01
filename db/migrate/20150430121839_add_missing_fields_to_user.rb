fb3dfb9b582c4840d23bf38dc695a2b5a34df522
Stephan Preeker <stephan@preeker.net>
2015-05-01 10:29:26 +0200

    fix schema

--- db/migrate/20150430121839_add_missing_fields_to_user.rb
+++ db/migrate/20150430121839_add_missing_fields_to_user.rb
@@ -11,7 +11,7 @@ class AddMissingFieldsToUser < ActiveRecord::Migration
     add_index :users, :email,                :unique => true
     add_index :users, :confirmation_token,   :unique => true
     add_index :users, :reset_password_token, :unique => true
-    add_index :users, :unlock_token,         :unique => true
+    #add_index :users, :unlock_token,         :unique => true
 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -538,42 +538,44 @@ ActiveRecord::Schema.define(version: 20150430121839) do
   add_index "tolk_translations", ["phrase_id", "locale_id"], name: "index_tolk_translations_on_phrase_id_and_locale_id", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",             limit: 255
-    t.string   "name",                 limit: 255
-    t.string   "email",                limit: 255
-    t.boolean  "confirmed",            limit: 1,   default: false, null: false
-    t.string   "crypted_password",     limit: 255
-    t.string   "password_salt",        limit: 255
-    t.string   "persistence_token",    limit: 255
-    t.string   "single_access_token",  limit: 255
-    t.string   "perishable_token",     limit: 255
-    t.integer  "login_count",          limit: 4,   default: 0,     null: false
-    t.integer  "failed_login_count",   limit: 4,   default: 0,     null: false
+    t.string   "username",               limit: 255
+    t.string   "name",                   limit: 255
+    t.string   "email",                  limit: 255
+    t.boolean  "confirmed",              limit: 1,   default: false, null: false
+    t.string   "crypted_password",       limit: 255
+    t.string   "password_salt",          limit: 255
+    t.string   "persistence_token",      limit: 255
+    t.string   "single_access_token",    limit: 255
+    t.string   "perishable_token",       limit: 255
+    t.integer  "login_count",            limit: 4,   default: 0,     null: false
+    t.integer  "failed_login_count",     limit: 4,   default: 0,     null: false
     t.datetime "last_request_at"
     t.datetime "current_login_at"
     t.datetime "last_login_at"
-    t.string   "current_login_ip",     limit: 255
-    t.string   "last_login_ip",        limit: 255
+    t.string   "current_login_ip",       limit: 255
+    t.string   "last_login_ip",          limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "address",              limit: 255
-    t.string   "postalcode",           limit: 255
-    t.string   "city",                 limit: 255
-    t.string   "telephone",            limit: 255
+    t.string   "address",                limit: 255
+    t.string   "postalcode",             limit: 255
+    t.string   "city",                   limit: 255
+    t.string   "telephone",              limit: 255
     t.date     "birth_date"
-    t.string   "birth_city",           limit: 255
-    t.string   "encrypted_password",   limit: 255
+    t.string   "birth_city",             limit: 255
+    t.string   "encrypted_password",     limit: 255, default: ""
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",        limit: 4,   default: 0
+    t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",   limit: 255
-    t.string   "last_sign_in_ip",      limit: 255
-    t.string   "confirmation_token",   limit: 255
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "reset_password_token", limit: 255
-    t.string   "remember_token",       limit: 255
+    t.string   "unconfirmed_email",      limit: 255
+    t.string   "remember_token",         limit: 255
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
