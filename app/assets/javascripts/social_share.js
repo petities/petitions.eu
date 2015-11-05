310a06cf732908266305b43e419fe12c157a863f
Stephan Preeker <stephan@preeker.net>
2015-11-05 16:41:31 +0100

    edit petition form improvements and little todo items

--- app/assets/javascripts/social_share.js
+++ app/assets/javascripts/social_share.js
@@ -7,9 +7,9 @@ function fbShare(url, title, descr, image, winWidth, winHeight) {
 
 window.twttr = (function (d,s,id) {
     var t, js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
-    js.src="//platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
-    return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+    //if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+    //js.src="//platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+    //return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
     }(document, "script", "twitter-wjs"));
 
 function gPlusShare(url) {
