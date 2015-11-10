5793b8f4a247c160246e18595eb6cd619f9230f3
Stephan Preeker <stephan@preeker.net>
2015-11-10 12:17:52 +0100

    i18n fallback

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module PetitionApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
     config.i18n.enforce_available_locales = false
+    config.i18n.fallbacks = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
