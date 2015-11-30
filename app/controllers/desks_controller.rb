d561f10ca0027c20bbb509b848e60a8779a891ca
Stephan Preeker <stephan@preeker.net>
2015-11-30 13:16:18 +0100

    fix #35

--- app/controllers/desks_controller.rb
+++ app/controllers/desks_controller.rb
@@ -17,17 +17,17 @@ class DesksController < ApplicationController
     @results_size = @petitions.size
 
     # find petition in state of allow
-    @petitions_allow = @petitions.where("status IN ('draft', 'concept', 'staging')")
+    @petitions_allow = @petitions.where("status IN ('draft', 'concept', 'staging')").limit(20)
     # find petitions in state of answer
-    @petitions_answer = @petitions.where("status IN ('to_process', 'in_process')")
+    @petitions_answer = @petitions.where("status IN ('to_process', 'in_process')").limit(20)
     # find petition in state of signable
-    @petitions_live = @petitions.live
+    @petitions_live = @petitions.live.limit(20)
     # find petition in state of answered
-    @petitions_completed = @petitions.where(status: 'completed')
+    @petitions_completed = @petitions.where(status: 'completed').limit(20)
     # find petition in state of done/ingetrokken
-    @petitions_rejected = @petitions.where(status: 'rejected')
+    @petitions_rejected = @petitions.where(status: 'rejected').limit(20)
     # withdrawn..
-    @petitions_withdrawn = @petitions.where(status: 'withdrawn')
+    @petitions_withdrawn = @petitions.where(status: 'withdrawn').limit(20)
     # @petitions = @petitions.paginate(page: @page, per_page: 12)
     #
     @users = User.order(:email)
