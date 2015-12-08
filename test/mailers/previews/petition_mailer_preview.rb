b77c8cbdec449fdc13a2f8a381b23b8b48ec9ee3
ReindeR Rustema <reinder@rustema.nl>
2015-12-08 08:38:00 +0100

    bunch of mails defined

--- test/mailers/previews/petition_mailer_preview.rb
+++ test/mailers/previews/petition_mailer_preview.rb
@@ -13,8 +13,51 @@ class PetitionMailerPreview < ActionMailer::Preview
     PetitionMailer.warning_due_date_mail(Petition.live.first)
   end
 
-  def due_date_ask_for_answer
-    PetitionMailer.due_date_ask_for_answer(Petition.live.first)
+  def write_about_hand_over_mail
+    PetitionMailer.write_about_hand_over_mail(Petition.live.first)
   end
  
+  def improve_and_reopen_mail
+    PetitionMailer.improve_and_reopen_mail(Petition.live.first)
+  end
+  
+  def petition_announcement_mail
+    PetitionMailer.petition_announcement_mail(Petition.live.first)
+  end
+
+  def announcement_reminder_mail
+    PetitionMailer.announcement_reminder_mail(Petition.live.first)
+  end
+
+  def default_signatories_answer_mail
+    PetitionMailer.default_signatories_answer_mail(Petition.live.first)
+  end
+
+  def process_explanation_mail
+    PetitionMailer.process_explanation_mail(Petition.live.first)
+  end
+
+  def virtual_hand_over_mail
+    PetitionMailer.virtual_hand_over_mail(Petition.live.first)
+  end
+
+  def handed_over_signatories_mail
+    PetitionMailer.handed_over_signatories_mail(Petition.live.first)
+  end
+
+  def answer_due_date_request_mail
+    PetitionMailer.answer_due_date_request_mail(Petition.live.first)
+  end
+
+  def answer_request_mail
+    PetitionMailer.answer_request_mail(Petition.live.first)
+  end
+
+  def adoption_request_signatory_mail
+    PetitionMailer.adoption_request_signatory_mail(Petition.live.first)
+  end
+
+  def adoption_result_signatories_mail
+    PetitionMailer.adoption_result_signatories_mail(Petition.live.first)
+  end
 end
