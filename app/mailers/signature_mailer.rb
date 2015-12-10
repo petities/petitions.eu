bdfe001da68137b676e4333552ad17293a612583
ReindeR Rustema <reinder@rustema.nl>
2015-12-10 14:04:35 +0100

    mailafriend and confirmation mail
    and some small preview mail settings

--- app/mailers/signature_mailer.rb
+++ app/mailers/signature_mailer.rb
@@ -39,7 +39,7 @@ class SignatureMailer < ApplicationMailer
     name = @signature.petition.name if @signature.petition.present?
     subject = t('mail.confirm.signature.subject', petition_name: name)
 
-    mail(to: @signature.person_email, subject: subject)
+    mail(from: 'bounces@petities.nl', reply_to: 'webmaster@petities.nl', to: @signature.person_email, subject: subject)
   end
 
   def share_mail(signature, target_email)
@@ -47,22 +47,25 @@ class SignatureMailer < ApplicationMailer
     # build the required globals for the template
     @signature = signature
     @petition = @signature.petition
-
+    @person_function = ''
+    if not @signature.person_function.nil?
+      @person_function = t('mail.mailafriend.note') + ' "%s"' % @signature.person_function
+    end
     # build a catch subject line
-    subject = t('confirm.info.mailafriend_subject', {
+    subject = t('mail.mailafriend.subject', {
       title: @petition.name})
 
     # build the mail
-    mail(to: target_email, subject: subject)
+    mail(from: 'bounces@petities.nl', reply_to: 'webmaster@petities.nl', to: target_email, subject: subject)
   end
 
   def inform_user_of_answer_mail(signature, petition, answer)
-    @signature = signatere
+    @signature = signature
     @petition = petition
     @answer = answer
 
     subject = t('signature.petition.is_answered')
 
-    mail(to: signature.person_email, subject: subject)
+    mail(from: 'bounces@petities.nl', reply_to: 'webmaster@petities.nl', to: signature.person_email, subject: subject)
   end
 end
