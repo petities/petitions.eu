182999a42925e7b5deafdc628e1c0c846665b097
Johan Kok <info@johankok.nl>
2015-12-16 10:01:51 +0100

    Modified unicorn settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,14 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
 namespace :deploy do
 
   after :restart, :clear_cache do
