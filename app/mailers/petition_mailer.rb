1302650fe3780997152bafa1e2515f21d719a169
ReindeR Rustema <reinder@rustema.nl>
2015-12-21 17:00:49 +0100

    petition mailer in sync with the views

--- app/mailers/petition_mailer.rb
+++ app/mailers/petition_mailer.rb
@@ -21,18 +21,9 @@ class PetitionMailer <  ApplicationMailer
     mail(to: @signature.person_email, subject: subject)
   end
 
-  # ask office again to answer
-  def announcement_reminder_mail(petition)
-
-    @petition = petition
-    @office = petition.office
-    target = @petition.office.email
-    subject = t('mail.request.announcement_subject')
-    mail(to: target, subject: subject )
-  end
-
   # ask office which date petition should get an answer
-  def answer_due_date_request_mail(petition)
+  def ask_office_answer_due_date_mail(petition)
+    Logger.debug('build ask for answer due date mail..')
 
     @office = petition.office
 
@@ -41,6 +32,16 @@ class PetitionMailer <  ApplicationMailer
     @petition = petition
 
     mail(to: @petition.office.email, subject: subject)
+  end
+
+  # ask office for answer to petition
+  def ask_office_for_answer_mail(petition)
+    Logger.debug('build ask for answer mail..')
+
+    subject = t('petition.moderation.we_need_answer')
+
+    mail(to: @petition.office.email, subject: subject)
+
   end
 
   # call petitioner into action about closing petition
@@ -66,6 +67,23 @@ class PetitionMailer <  ApplicationMailer
 
   end
 
+  # signatory gets the answer to the petition
+  def inform_user_of_answer_mail(signature, petition, answer)
+    @signature = signature
+    @petition = petition
+    @answer = answer
+    @unique_key = url_for(
+      controller: 'signatures',
+      action: 'confirm',
+      host: 'petities.nl',
+      signature_id: @signature.unique_key)
+
+    subject = t('mail.petition.is_answered', {
+      title: @petition.name})
+
+    mail(from: 'bounces@petities.nl', reply_to: 'webmaster@petities.nl', to: signature.person_email, subject: subject)
+  end
+
   # announce petition to office
   def petition_announcement_mail(petition)
 
@@ -74,6 +92,20 @@ class PetitionMailer <  ApplicationMailer
     target = @petition.office.email
     subject = t('mail.request.announcement_subject')
     mail(to: target, subject: subject )
+  end
+
+  # ask office for reference number
+  def reference_number_mail(petition, target="")
+    Logger.debug('building reference number mail..')
+    @petition = petition
+    @office = petition.office
+    target = @petition.office.email
+    subject = t('mail.request.announcement_subject')
+    mail(to: target, subject: subject )
+
+    if not target
+      target = @petition.office.email
+    end
   end
 
   # each petition status change by e-mail to admin
@@ -93,40 +125,6 @@ class PetitionMailer <  ApplicationMailer
     end
   end
 
-  # ask office for reference number
-  def reference_number_mail(petition, target="")
-    Logger.debug('building reference number mail..')
-
-    subject = t('petition.moderation.we_need_reference')
-
-    if not target
-      target = @petition.office.email
-    end
-
-    mail(to: target, subject: subject)
-
-  end
-
-  # ask office when petition gets answer
-  def office_ask_for_answer_due_date_mail(petition)
-    Logger.debug('build ask for answer due date mail..')
-
-    subject = t('petition.moderation.we_need_answer')
-
-    mail(to: @petition.office.email, subject: subject)
-
-  end
-
-  # ask office for answer to petition
-  def office_ask_for_answer_mail(petition)
-    Logger.debug('build ask for answer mail..')
-
-    subject = t('petition.moderation.we_need_answer')
-
-    mail(to: @petition.office.email, subject: subject)
-
-  end
-
   # ask petitioner to confirm, give user and password
   def welcome_petitioner_mail(petition, user, password)
 
@@ -143,21 +141,4 @@ class PetitionMailer <  ApplicationMailer
     mail(to: user.email, subject: subject)
   end
   
-  # signatory gets the answer to the petition
-  def inform_user_of_answer_mail(signature, petition, answer)
-    @signature = signature
-    @petition = petition
-    @answer = answer
-    @unique_key = url_for(
-      controller: 'signatures',
-      action: 'confirm',
-      host: 'petities.nl',
-      signature_id: @signature.unique_key)
-
-    subject = t('mail.petition.is_answered', {
-      title: @petition.name})
-
-    mail(from: 'bounces@petities.nl', reply_to: 'webmaster@petities.nl', to: signature.person_email, subject: subject)
-  end
-
 end
