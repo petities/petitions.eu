7f643968994ccc2aa88de1833055ed1fb7bbd50a
Stephan Preeker <stephan@preeker.net>
2016-01-31 16:15:20 +0100

    Revert "Revert "improve usability by indicating read only fields" because it breaks tests"
    This reverts commit d67c67cdc997b82281ade8e1f61fefccf0fb50d2.

--- app/policies/petition_policy.rb
+++ app/policies/petition_policy.rb
@@ -53,7 +53,8 @@ class PetitionPolicy < ApplicationPolicy
     unless user.has_role?(:admin, petition.office)
       unless petition.is_draft? or petition.is_staging?
          remove += [
-          :name, :subdomain,
+          :name, 
+          :subdomain,
           :initiators, 
           :statement,
           :request,                      
