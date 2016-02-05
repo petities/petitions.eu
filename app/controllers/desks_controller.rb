7d4e2caaecb79ad0d820d1795140a240341cbb1b
Stephan Preeker <stephan@preeker.net>
2016-02-05 04:05:21 +0100

    use redis counts everywhere

--- app/controllers/desks_controller.rb
+++ app/controllers/desks_controller.rb
@@ -12,8 +12,12 @@ class DesksController < ApplicationController
       @office = Office.find_by_subdomain(request.subdomain)
     end
 
-    @signatures_count = Signature.joins(:petition)
-                        .where(petitions: { office_id: @office.id }).size
+    @signatures_count = 0
+    #@signatures_count = Signature.joins(:petition)
+    #                    .where(petitions: { office_id: @office.id }).size
+    Petition.where(office_id: @office.id).all.each do |p|
+      @signatures_count += p.get_count
+    end
 
     if user_signed_in? && current_user.has_role?(:admin, @office)
       show_office_page
