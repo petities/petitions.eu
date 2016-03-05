ea892fc18ff040362418fe5aebb296afef85c706
ReindeR Rustema <reinder@rustema.nl>
2016-03-05 19:07:11 +0100

    fix typo in model

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -122,7 +122,7 @@ class Petition < ActiveRecord::Base
 
   # petitionaris
   PETITIONARIS = [
-    [t('petition.stageing'), 'stageing'], # offer for review
+    [t('petition.staging'), 'staging'], # awaiting moderation
   ]
 
   scope :live,      -> { where(status: 'live') }
