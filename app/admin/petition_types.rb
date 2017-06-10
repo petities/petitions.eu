791ce22dec47bb27bca59ca5ace8c0b3be53af83
Simon Koolstra <skoolstra@users.noreply.github.com>
2017-06-10 11:11:57 +0200

    Save all custom fields for a PetitionType. References #396

--- app/admin/petition_types.rb
+++ app/admin/petition_types.rb
@@ -2,10 +2,20 @@ ActiveAdmin.register PetitionType do
   permit_params :name, :description, :required_minimum_age,
                 :display_person_born_at, :display_person_birth_city,
                 :require_person_born_at, :require_person_birth_city,
-                :display_signature_person_citizen, :country_code,
-                :display_signature_full_address, :require_signature_full_address
+                :display_signature_person_citizen,
+                :display_signature_full_address,
+                :require_signature_full_address,
+                :custom_additional_info_visible,
+                :custom_additional_info_person_name,
+                :custom_additional_info_person_street,
+                :custom_additional_info_person_postalcode,
+                :custom_additional_info_person_function,
+                :custom_additional_info_subscribe,
+                :custom_additional_info_person_birth_date,
+                :custom_additional_info_person_birth_city,
+                :custom_additional_info_person_dutch_citizen,
+                :custom_additional_info_more_information, :country_code
 
-  # config.petitions = false
   filter :name
   filter :description
   filter :required_minimum_age
--- test/admin/petition_types_controller_test.rb
+++ test/admin/petition_types_controller_test.rb
@@ -17,6 +17,24 @@ module Admin
       assert_response :success
     end
 
+    test 'should get new' do
+      get :new
+      assert_response :success
+    end
+
+    test 'should create' do
+      input_values = petition_types(:custom_texts).attributes
+      post :create, petition_type: input_values
+      petition_type = assigns(:petition_type)
+      assert_redirected_to admin_petition_type_path(petition_type)
+
+      petition_type.attributes
+                   .except('id', 'created_at', 'updated_at')
+                   .each do |key, value|
+        assert_equal input_values[key], value
+      end
+    end
+
     test 'should get edit' do
       get :edit, id: @petition_type.id
       assert_response :success
