fefd18b3ef006cc92544d6d54d03b074a128feda
Simon Koolstra <skoolstra@users.noreply.github.com>
2017-08-26 17:58:24 +0200

    Display share buttons on mobile, changed appearance
    * Replaced images with fontawesome
    * Resolved #282
    * References #39

--- app/assets/javascripts/social_share.js
+++ app/assets/javascripts/social_share.js
@@ -1,23 +1,39 @@
 // FACEBOOK
-function fbShare(url, title) {
+function fbShare (url, title) {
   openShareWindow('https://www.facebook.com/sharer.php?s=100&p[title]=' + encodeURIComponent(title) + '&p[url]=' + encodeURIComponent(url), 520, 350);
 }
 
 // Twitter
-function twitterShare(url, title){
+function twitterShare (url, title){
   openShareWindow('http://twitter.com/share?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(title) + '&', 450, 550);
 }
 
 // Google plus
-function gPlusShare(url) {
+function gPlusShare (url) {
   openShareWindow('https://plus.google.com/share?url=' + encodeURIComponent(url), 500, 350);
 }
 
-function lnShare(url) {
+function lnShare (url) {
   openShareWindow('https://www.linkedin.com/cws/share?url=' + encodeURIComponent(url), 500, 350);
 }
 
-function openShareWindow(url, width, height) {
+$(function() {
+  $('a.share-button.link').on('click', function (event) {
+    var $this = $(this);
+    event.preventDefault();
+
+    if ($this.hasClass('share-button-url-visible')) {
+      $this.removeClass('share-button-url-visible');
+      $('.share-button-url').parent().remove();
+    } else {
+      $this.addClass('share-button-url-visible');
+      $this.after('<div class="center"><input type="text" class="share-button-url" value="' + this.href + '"></div>')
+      $('.share-button-url').select();
+    };
+  });
+});
+
+function openShareWindow (url, width, height) {
   var winTop = (screen.height / 2) - (width / 2);
   var winLeft = (screen.width / 2) - (height / 2);
 
