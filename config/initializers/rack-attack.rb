d45be86c5948c1c2fcfe435e8e747a5162ed49d6
Simon Koolstra <skoolstra@users.noreply.github.com>
2018-09-08 13:14:56 +0200

    Limit posting to the contact form

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -2,4 +2,10 @@ class Rack::Attack
   throttle("req/ip", limit: 300, period: 5.minutes) do |req|
     req.ip
   end
+
+  throttle('limit contact form', limit: 3, period: 15.minutes) do |req|
+    if req.path == '/contact' && req.post?
+      req.ip
+    end
+  end
 end
