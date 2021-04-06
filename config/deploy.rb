3ddd0440d616cda1e43ad300bb62d13f3a34d8af
Simon Koolstra <skoolstra@users.noreply.github.com>
2021-04-06 19:05:47 +0200

    Bumped capistrano config to 3.16.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.11.0'
+lock '~> 3.16.0'
 
 set :application, 'petitions.eu'
 set :repo_url, 'git@github.com:petities/petitions.eu.git'
