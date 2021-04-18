433f88cb3272f25f36118a882a59b1f00e6e0ac7
Simon Koolstra <skoolstra@users.noreply.github.com>
2021-04-18 14:07:33 +0200

    Remove redis_analytics

--- config/initializers/redis.rb
+++
@@ -1,7 +0,0 @@
-require 'redis_analytics'
-
-# configure your redis_connection (mandatory) and redis_namespace (optional)
-RedisAnalytics.configure do |configuration|
-  configuration.redis_connection = Redis.new
-  configuration.redis_namespace = 'ra'
-end
