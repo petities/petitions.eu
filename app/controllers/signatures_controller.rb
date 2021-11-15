78650c4e5f66746412b23bef9d5f7c7dfcd06cdd
Simon Koolstra <skoolstra@users.noreply.github.com>
2021-11-15 21:32:40 +0100

    Move display_donations? check to server-side

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -74,7 +74,7 @@ class SignaturesController < ApplicationController
       # send confirmation mail again
       @signature.send(:send_confirmation_mail)
       respond_to do |format|
-        format.js { render json: { status: 'ok', petition: @petition } }
+        format.js { render json: { status: 'ok' } }
       end
       # DONE!
       return
@@ -89,7 +89,7 @@ class SignaturesController < ApplicationController
     # respond to json request
     respond_to do |format|
       if @signature.save
-        format.js { render json: { status: 'ok', petition: @petition } }
+        format.js { render json: { status: 'ok' } }
       else
         format.js { render json: @signature.errors, status: :unprocessable_entity }
       end
