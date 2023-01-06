e98228f683296ca30f67e61f0a96e744d6564d74
Simon Koolstra <skoolstra@users.noreply.github.com>
2023-01-06 09:38:04 +0100

    Updated static donations URL

--- test/controllers/donations_controller_test.rb
+++ test/controllers/donations_controller_test.rb
@@ -4,6 +4,6 @@ class DonationsControllerTest < ActionController::TestCase
   test 'should redirect index to payment request' do
     get :index
     assert_response :redirect
-    assert_redirected_to 'https://betaalverzoek.rabobank.nl/betaalverzoek/?id=rDI_dpAhQGalApzWyB8DPQ'
+    assert_redirected_to 'https://betaalverzoek.rabobank.nl/betaalverzoek/?id=MX3AG14USwamtfVmuwZxng'
   end
 end
